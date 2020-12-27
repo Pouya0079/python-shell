fcfa37f74d9e27695506fe22a10e302867cb2bb1
Pouya Nekou <pouya.nekou@gmail.com>
2020-12-27 14:53:47 +0330

    use some statich methods

--- shell.py
+++ shell.py
@@ -13,16 +13,53 @@ class Shell:
     @staticmethod
     def set_screen():
         """Set color screen and clear it"""
+
         print('\033[1;32;40m')
         os.system('clear')
 
     @staticmethod
     def shell_figlet():
         """Set a figlet text for shell"""
+
         fgl = Figlet(font='slant')
         print(fgl.renderText('Python Shell'))
 
-    def change_directory(self, path):
+    @staticmethod
+    def execute_pipe_command(command):
+        """Method for executing pipe commands"""
+
+        tmp_in, tmp_out = (0, 0)
+        tmp_in = os.dup(0)
+        tmp_out = os.dup(1)
+
+        fdin = os.dup(tmp_in)
+
+        for cmd in command.split('|'):
+            os.dup2(fdin, 0)
+            os.close(fdin)
+
+            if cmd == command.split('|')[-1]:
+                fdout = os.dup(tmp_out)
+
+            else:
+                fdin, fdout = os.pipe()
+
+            os.dup2(fdout, 1)
+            os.close(fdout)
+
+            try:
+                sp.run(cmd.strip().split())
+
+            except Exception:
+                print(f'\033[1;31;40mError: command not found: {cmd}')
+
+        os.dup2(tmp_in, 0)
+        os.dup2(tmp_out, 1)
+        os.close(tmp_in)
+        os.close(tmp_out)
+
+    @staticmethod
+    def change_directory(path):
         """Method for changing directories"""
 
         try:
@@ -36,35 +73,7 @@ class Shell:
 
         try:
             if '|' in command:
-                tmp_in, tmp_out = (0, 0)
-                tmp_in = os.dup(0)
-                tmp_out = os.dup(1)
-
-                fdin = os.dup(tmp_in)
-
-                for cmd in command.split('|'):
-                    os.dup2(fdin, 0)
-                    os.close(fdin)
-
-                    if cmd == command.split('|')[-1]:
-                        fdout = os.dup(tmp_out)
-
-                    else:
-                        fdin, fdout = os.pipe()
-
-                    os.dup2(fdout, 1)
-                    os.close(fdout)
-
-                    try:
-                        sp.run(cmd.strip().split())
-
-                    except Exception:
-                        print(f'\033[1;31;40mError: command not found: {cmd}')
-
-                os.dup2(tmp_in, 0)
-                os.dup2(tmp_out, 1)
-                os.close(tmp_in)
-                os.close(tmp_out)
+                self.execute_pipe_command(command)
 
             else:
                 sp.run(command.split())
